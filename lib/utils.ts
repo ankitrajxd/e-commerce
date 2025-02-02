c35384b694d5e269b1068371fc7335614310f096
Ankit <ankitrajxd@gmail.com>
2025-02-02 22:50:57 +0530

    zod validation and type inference

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,9 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
+
+// format number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [integer, decimal] = num.toString().split(".");
+  return decimal ? `${integer}.${decimal.padEnd(2, "0")}` : `${integer}.00`;
+}
