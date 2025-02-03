30d3e9dbe08b64a7834296a76696bb2d869d36d2
Ankit <ankitrajxd@gmail.com>
2025-02-03 19:53:50 +0530

    add user actions

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { signIn, signOut } from "@/auth";
+import { signInFormSchema } from "../validators";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
+
+// sign in users with credentials
+export async function signInWithCredentials(
+  prevState: unknown,
+  formdata: FormData
+) {
+  try {
+    const user = signInFormSchema.parse({
+      email: formdata.get("email"),
+      password: formdata.get("password"),
+    });
+
+    await signIn("credentials", user);
+    return {
+      success: true,
+      message: "Sign in successful",
+    };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return {
+      success: false,
+      message: "Invalid email or password",
+    };
+  }
+}
+
+// sign out users
+export async function signOutUser() {
+  await signOut();
+}
