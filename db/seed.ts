e6c80c1c342be8ab20e69d47e056ab5903816f79
Ankit <ankitrajxd@gmail.com>
2025-02-03 19:10:47 +0530

    seed users

--- db/seed.ts
+++ db/seed.ts
@@ -1,13 +1,27 @@
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@/db/prisma";
 import sampleData from "./sample-data";
 
-async function main() {
-  const prisma = new PrismaClient();
-  await prisma.product.deleteMany();
-  await prisma.product.createMany({
-    data: sampleData.products,
+// async function main() {
+//   await prisma.product.deleteMany();
+//   await prisma.product.createMany({
+//     data: sampleData.products,
+//   });
+//   console.log("Db seeded successfully!");
+// }
+
+// seeding users
+async function seedUsers() {
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verificationToken.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.user.deleteMany();
+
+  await prisma.user.createMany({
+    data: sampleData.users,
   });
-  console.log("Db seeded successfully!");
+
+  console.log("Users seeded successfully!");
 }
 
-main();
+seedUsers();
