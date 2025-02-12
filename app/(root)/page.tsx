40d7a8a446ea7a24c9097df3870a9366853c7148
Ankit <ankitrajxd@gmail.com>
2025-02-12 22:24:54 +0530

    configured eslint rules

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,7 +5,6 @@ import React from "react";
 
 const HomePage = async () => {
   const latestProduct = await getLatestProducts();
-
   return (
     <div>
       <ProductList data={latestProduct} title="Latest Products" />
